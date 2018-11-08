367281fb011b642b0cf301d5bab981260a097dc6
Justin Lim <justilim@gmail.com>
2018-11-08 00:48:27 -0800

    add user creation functionality

--- login.py
+++ login.py
@@ -7,6 +7,7 @@ username and password and verifies the credentials.
 import tkinter as tk
 from tkinter import ttk
 from tkinter import font as tkfont
+from user import UserManager
 
 class SampleApp(tk.Tk):
     """Controller for the login screen
@@ -15,9 +16,6 @@ class SampleApp(tk.Tk):
     authentication and routes the user according to whether or not authentication
     was successful.
     """
-
-    _valid_username = "root"
-    _valid_password = "password"
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
@@ -31,7 +29,8 @@ class SampleApp(tk.Tk):
         container.grid_rowconfigure(0, weight=1)
 
         self.frames = {}
-        for page in (LoginPage, LoggedInPage, LoginFailPage):
+        for page in (LoginPage, LoggedInPage, LoginFailPage,
+                     CreateNewUserPage):
             page_name = page.__name__
             frame = page(parent=container, controller=self)
             self.frames[page_name] = frame
@@ -40,6 +39,11 @@ class SampleApp(tk.Tk):
 
         frame = self.frames["LoginPage"]
         frame.tkraise()
+
+        """Database collection."""
+        self.db = UserManager("./users.db")
+        """Create an initial user."""
+        self.create_new_user("root", "password")
 
     def authenticate(self, username, password):
         """Authenticates a username and password.
@@ -52,19 +56,36 @@ class SampleApp(tk.Tk):
         password - password to verify
         """
 
-        if (username == self._valid_username and
-                password == self._valid_password):
-
+        if (self.db.verifyPassword(username, password)):
             frame = self.frames["LoggedInPage"]
             frame.tkraise()
         else:
             frame = self.frames["LoginFailPage"]
             frame.tkraise()
 
+    def create_new_user(self, username, password):
+        """Creates a new user.
+
+        Creates a new user and adds the credentials to a database if the
+        username does not already exists.
+
+        Args:
+        username - username of the new user
+        password - password of the new user
+        """
+        self.db.createUser(username, password)
+
+
     def go_to_login(self):
         """ Presents the login page."""
 
         frame = self.frames["LoginPage"]
+        frame.tkraise()
+
+    def go_to_create_user(self):
+        """Presents the page to create new users."""
+
+        frame = self.frames["CreateNewUserPage"]
         frame.tkraise()
 
 class LoginPage(ttk.Frame):
@@ -98,9 +119,37 @@ class LoggedInPage(ttk.Frame):
         self.controller = controller
         label = ttk.Label(self, text="Success!", font=controller.title)
         label.pack(side="top", fill="x", pady=10)
-        button = ttk.Button(self, text="Logout",
+        logout_btn = ttk.Button(self, text="Logout",
                             command=lambda: controller.go_to_login())
-        button.pack()
+        create_user_btn = ttk.Button(self, text="Logout",
+                            command=lambda: controller.go_to_create_user())
+        logout_btn.pack()
+        create_user_btn.pack()
+
+
+class CreateNewUserPage(ttk.Frame):
+    """Constructs page for registering new users."""
+
+    def __init__(self, parent, controller):
+        ttk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        username = tk.StringVar()
+        password = tk.StringVar()
+
+        username_entry = ttk.Entry(self, width=7, textvariable=username)
+        username_entry.grid(column=2, row=1, sticky=("we"))
+
+        password_entry = ttk.Entry(self, width=7, show="*", textvariable=password)
+        password_entry.grid(column=2, row=2, sticky=("we"))
+
+        ttk.Label(self, text="User Name").grid(column=1, row=1, sticky="w")
+        ttk.Label(self, text="Password").grid(column=1, row=2, sticky="w")
+
+        submit_btn = ttk.Button(self, text="Create User", command=lambda:
+                                controller.create_new_user(username, password))
+        submit_btn.grid(column=2, row=3, sticky="es")
+
 
 class LoginFailPage(ttk.Frame):
     """Constructs the page for failed logins."""
